3eab3e022497f7c707693a8a2d9f00645db0a61c
Harshit gupta <guptaharshit545@gmail.com>
2020-08-02 11:49:45 +0530

    covid-19 app first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import "./App.css";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import Table from "./Table";
+import LineGraph from "./LineGraph";
+import { sortData, prettyPrintStat } from "./util";
+import "leaflet/dist/leaflet.css";
 
 function App() {
+  // Use state
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  // const [mapCountries, setMapCountries] = useState([]);
+  const [caseType, setCaseType] = useState("cases");
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // United state
+            value: country.countryInfo.iso2, //uk, usa ,
+          }));
+          // console.log(countries);
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          // setMapCountries(data);
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    const url =
+      countryCode === "worldwide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
+        console.log(data);
+      });
+  };
+  // console.log(countryInfo);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_left">
+        <div className="app_header">
+          <h1>COVID 19 TRACKER</h1>
+          <FormControl className="app_dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              {/* {Loop through all the countries and show a dropdown list of the options} */}
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((countrie) => (
+                <MenuItem value={countrie.value}>{countrie.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app_stats">
+          <InfoBox
+            isRed
+            active={caseType === "cases"}
+            onClick={(e) => setCaseType("cases")}
+            title="Cases"
+            cases={prettyPrintStat(countryInfo.todayCases)}
+            total={prettyPrintStat(countryInfo.cases)}
+          />
+          <InfoBox
+            active={caseType === "recovered"}
+            onClick={(e) => setCaseType("recovered")}
+            title="Recoverd"
+            cases={prettyPrintStat(countryInfo.todayRecovered)}
+            total={prettyPrintStat(countryInfo.recovered)}
+          />
+          <InfoBox
+            isRed
+            active={caseType === "deaths"}
+            onClick={(e) => setCaseType("deaths")}
+            title="Deaths"
+            cases={prettyPrintStat(countryInfo.todayDeaths)}
+            total={prettyPrintStat(countryInfo.deaths)}
+          />
+        </div>
+
+        <Map
+          casesType={caseType}
+          // countries={mapCountries}
+          center={mapCenter}
+          zoom={mapZoom}
+        />
+      </div>
+      <Card className="app_right">
+        <CardContent>
+          <h3>Live Cases by country</h3>
+          <Table countries={tableData} />
+          <h3 className="app_grpahTitle">Worldwide new {caseType}</h3>
+          <LineGraph className="app_graph" casesType={caseType} />
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/util.js
@@ -0,0 +1,50 @@
+// import React from "react";
+// import { Circle, Popup } from "leaflet";
+import numeral from "numeral";
+
+// const casesTypeColors = {
+//     cases: {
+//         hex: "#cc1034",
+//         multiplier: 800,
+//     },
+//     recovered: {
+//         hex: "#7dd71d",
+//         multiplier: 1200,
+//     },
+//     deaths: {
+//         hex: "#fb4443",
+//         multiplier: 2000,
+//     },
+// };
+
+export const sortData = (data) => {
+    const sortedData = [...data];
+    // sortedData.sort((a, b) => {
+    //     if(a.cases > b.cases) {
+    //         return -1;
+    //     }else {
+    //         return 1;
+    //     }
+    // });
+    // return sortedData;
+    return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
+};
+
+// export const showDataOnMap = (data, casesType = 'cases') => 
+//     data.map((country) => (
+//         <Circle 
+//             center={[country.countryInfo.lat, country.countryInfo.long]}
+//             fillOpacity={0.4}
+//             color={casesTypeColors[casesType].hex}
+//             fillColor={casesTypeColors[casesType].hex}
+//             radius={
+//                 Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+//             }
+//         >
+//             <Popup>
+//                 <h1>i am popup</h1>
+//             </Popup>
+//         </Circle>
+//     ))
+
+export const prettyPrintStat = (stat) => stat ? `+${numeral(stat).format("0.0a")}` : "+0";
